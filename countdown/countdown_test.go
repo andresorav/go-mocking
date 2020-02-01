e09ca3f6dae57991bac4f66a097e807a684f61c1
Andres Orav <andres.orav@pipedrive.com>
2020-02-01 17:38:16 +0200

    Test sleep and write order in Countdown

--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -2,11 +2,25 @@ package countdown
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 type SpySleeper struct {
 	Calls int
+}
+
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, "sleep")
+}
+
+func (s *CountdownOperationSpy) Write(_ []byte) (n int, err error) {
+	s.Calls = append(s.Calls, "write")
+	return
 }
 
 func (s *SpySleeper) Sleep() {
@@ -32,5 +46,21 @@ Go!
 
 	if spySleeper.Calls != 4 {
 		t.Errorf("Not enought calls to sleeper, want 4 got %d", spySleeper.Calls)
+	}
+}
+
+func TestSleepBeforeEveryWrite(t *testing.T) {
+	spySleepPrinter := &CountdownOperationSpy{}
+	Countdown(spySleepPrinter, spySleepPrinter)
+
+	want := []string{
+		"sleep", "write",
+		"sleep", "write",
+		"sleep", "write",
+		"sleep", "write",
+	}
+
+	if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+		t.Errorf("Wanted calls %v got %v", want, spySleepPrinter.Calls)
 	}
 }
